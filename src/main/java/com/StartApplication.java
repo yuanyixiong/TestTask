edc61c15bf704106e700c6a390857b05510b1826
袁毅雄 <15926499574@163.com>
2019-08-06 19:28:06 +0800

    提交项目

---
+++ src/main/java/com/StartApplication.java
@@ -0,0 +1,28 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 袁毅雄
+ * @description
+ * @date 2019/8/6
+ */
+@EnableAutoConfiguration
+@SpringBootApplication
+@RestController
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("hello");
+        return "hello";
+    }
+}
